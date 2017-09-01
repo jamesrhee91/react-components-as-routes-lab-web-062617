709bdd9266e215e02a208a55fc3a2b6fd3c91d74
jamesrhee91 <jamesshrhee@gmail.com>
2017-08-31 21:49:49 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div key={movie.title}>
+            <h4>{movie.title}</h4>
+            {Object.entries(movie).slice(1).map(att => {
+              return (
+                <ul key={att[0]}>
+                  <li>{att[0]} : {att[1]}</li>
+                </ul>
+                )
+              })}
+          </div>
+          )
+        })
+      }
     </div>
   );
 };
